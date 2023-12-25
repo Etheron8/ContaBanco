c81d2214c036c56e15bdf3f0fc516208bf167782
Etheron <40326651+Etheron8@users.noreply.github.com>
2023-12-25 15:24:03 -0300

    try/catch

--- src/FormatarConta.java
+++ src/FormatarConta.java
@@ -1,18 +1,19 @@
 public class FormatarConta {
-
     public static void main(String[] args) {
+
         try {
-            String suaConta = contaformatada("98598");
-            System.out.println(suaConta);
+            String newConta = contaFormatada("6519");
+            System.out.println("Sua Nova Conta é " + newConta);
         } catch (ContaException e) {
-            System.out.println("Sua conta deve 4 digitos numericos");
+            // TODO Auto-generated catch block
+            System.out.println("Sua Conta deve ter exatamente 4 Digitos numericos !!");
             e.printStackTrace();
         }
     }
 
-    static String contaformatada(String conta) throws ContaException {
+    static String contaFormatada(String conta) throws ContaException {
         if (conta.length() != 4)
             throw new ContaException();
-        return "5458";
+        return conta;
     }
 }
